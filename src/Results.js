41496c9c1b731c65de093d6eaeaddc6a1f14b7fa
kai10107 <kaiaustin@icloud.com>
2024-07-10 14:18:51 +1000

    adds results section

---
+++ src/Results.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Results() {
+  return (
+    <div>
+      <h1>Results</h1>
+      <p>Results content will go here.</p>
+    </div>
+  );
+}
+
+export default Results;
